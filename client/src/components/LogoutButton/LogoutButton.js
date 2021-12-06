345095e918ecb76726d4775494653e3555c15556
oumaima <naouioumaima20@gmail.com>
2021-12-06 16:11:42 +0100

    fix

--- client/src/components/LogoutButton/LogoutButton.js
+++ client/src/components/LogoutButton/LogoutButton.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { MDBBtn } from "mdbreact";
+import { useHistory } from "react-router";
 
 const LogoutButton = () => {
   const { logout, user } = useAuth0();
   const { email } = user;
  
   const navColor = {backgroundColor:'#8B0000' }
-
+  const history = useHistory()
   return <MDBBtn color="elegant"
             type="submit"
-            onClick={() => logout({ returnTo: window.location.origin, })}>
+            //onClick={() => logout({ returnTo: window.location.origin, })}
+            onClick={() => history.push("./")}>
               Logout: {email}
             {/* onClick={() => logout()}>Log Out */}
         </MDBBtn>;
